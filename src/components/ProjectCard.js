794c395fe05a699b550127798e90654e55617006
Jhanril <jhanrilbucod@gmail.com>
2023-04-03 02:03:02 +0800

    update card glowing border

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl, github, livelink, bcolor }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx" style={{boxShadow: `0px 0px 15px 0px ${bcolor}`}}>
+      <div className="proj-imgbx" style={{boxShadow: `0px 0px 17px 0px ${bcolor}`, border: `2px solid ${bcolor}`}}>
         <img src={imgUrl} alt="" style={{height: "217px", objectFit: "cover"}} />
         <div className="proj-txtx">
           <h4>{title}</h4>
